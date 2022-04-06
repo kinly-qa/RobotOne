dbe7c9239b2fe6723e3676f2f66e312dcc715af9
KenS <ken@bekinly.com>
2022-04-06 09:55:21 -0400

    updated Shop.py

--- customLibraries/Shop.py
+++ customLibraries/Shop.py
@@ -5,7 +5,7 @@ class Shop:
 
     @keyword
     def hello_world(self):
-        print("Hello")
+        print("Hello World!")
 
     @keyword
     def add_items_to_cart_and_checkout(self,product_list):
